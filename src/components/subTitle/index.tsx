e0bd62e77db8ceaaf372532828f1a5c4c7257f19
Liting <janetli82@gmail.com>
2026-08-11 07:43:55 +0200

    saved it temporarily

---
+++ src/components/subTitle/index.tsx
@@ -0,0 +1,9 @@
+const SubTitle = () => {
+  return(
+    <div>
+      <h2 className="text-3xl">Choose a city. Pick the must-haves. Avoid the extras. Get your packing score.</h2>
+    </div>
+  )
+}
+
+export default SubTitle
