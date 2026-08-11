14feb776b4e1b03bd1b7a8737b7ec4aa9800a21e
Liting <janetli82@gmail.com>
2026-08-11 12:27:22 +0200

    saved it temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 
+import ListItems from "@/components/ListItems";
+import { travelList } from "@/type/city";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedCity, setSelectedCity] = useState<string>("")
+  const findCity = travelList.find(item => item.name === selectedCity)
+  const [selectedLists, setSelectedLists] = useState<string[]>([])
+  const handleChange = (selectedCity:string) => {
+    setSelectedCity(selectedCity)
+  }
+ 
+
   return (
     <div>
-      test
+      {findCity && <ListItems selectedId={findCity.id} selectedLists={selectedLists} updateChangeBox={setSelectedLists}/>}
     </div>
   );
 }
