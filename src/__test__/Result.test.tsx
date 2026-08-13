4c87e0051e69738b771f98477d5a5e20203087f5
Liting <janetli82@gmail.com>
2026-08-13 19:33:34 +0200

    adding testing-result

--- src/__test__/Result.test.tsx
+++ src/__test__/Result.test.tsx
@@ -1,23 +1,23 @@
 import {render, screen} from "@testing-library/react"
-//import Result from ""
+import Result from ""
 
 describe("test the three possible outcomes of the result component", () => {
   test("displays correct score", () => {
-    //render(<Result />)
+    render(<Result travel={} checkedItems={} />)
     const showScore = screen.getByTestId("result")
     expect(showScore).toBeInTheDocument()
-    expect(showScore).toHaveTextContent(/congratulation/i)
+    expect(showScore).toHaveTextContent(/congratulation! You answered all of them correctly./i)
   })
   
   test("shows warning when user selects more than correct items", () => {
-    //render(<Result />)
+    render(<Result travel={} checkedItems={} />)
     const showWarning = screen.getByTestId("result")
     expect(showWarning).toBeInTheDocument()
-    expect(showWarning).toHaveTextContent(/you selects more than correct items/i)
+    expect(showWarning).toHaveTextContent(/Warning! You got all the correct answers, but you also selected extra ones. Challenge failed/i)
   })
 
   test("the user hasn't played yet", () => {
-    //render(<Result  />)
+    render(<Result travel={} checkedItems={} />)
     const noResult = screen.queryByTestId("result")
     expect(noResult).not.toBeInTheDocument()
    })
