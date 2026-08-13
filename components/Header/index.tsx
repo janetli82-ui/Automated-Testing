2d957afccd957370ba9ceece08b5f9c29ac4414c
wineynia <gabiybiy@gmail.com>
2026-08-13 04:09:17 +0200

    Add header

---
+++ components/Header/index.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="flex justify-center gap-3 p-4 mt-5">
+      <img
+        src="/packingList.jpg"
+        alt="Travel documents icon"
+        className="h-10 w-10"
+      />
+      <h1 className="text-3xl font-semibold">Are you ready to travel the world?</h1>
+    </header>
+  )
+}
+
+export default Header
