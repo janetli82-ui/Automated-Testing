4717dd9bab348debb7a75a5c7599a06ffbac935e
wineynia <gabiybiy@gmail.com>
2026-08-14 10:55:02 +0200

    Formatting

--- src/components/subTitle/subTitle.test.tsx
+++ src/components/subTitle/subTitle.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import SubTitle from '.'
 
 describe('subTitle', () => {
-  it("<it shows the SubTitle component", () => {
+  test("<it shows the SubTitle component", () => {
     render(<SubTitle />)
     expect(screen.getByTestId('subtitle')).toBeInTheDocument()
   })
