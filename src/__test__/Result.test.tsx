ef5b694b427be07c7a1af00c5455eaeffc5b3067
Liting <janetli82@gmail.com>
2026-08-14 15:43:16 +0200

    adding one result

--- src/__test__/Result.test.tsx
+++ src/__test__/Result.test.tsx
@@ -2,32 +2,57 @@ import {render, screen} from "@testing-library/react"
 import Result from "../components/Result"
 import { travelList } from "@/data/city"
 
-describe("test the three possible outcomes of the result component", () => {
+describe("test the 4 possible outcomes of the result component", () => {
   const mockCity = travelList[2]
-  const mockSelectedItems = ["Light jacket for cool evenings",
-      "Breathable blouses (2 for weekend / 5 for week)",
-      "Light trousers (1 for weekend / 2 for week)",
-      "Shorts (1 for weekend / 2 for week)",
-      "Underwear (3 for weekend / 7 for week)"]
+  const mockSelectedItems = ["Stylish summer tops (2 for weekend / 5 for week)",
+    "Linen trousers (1 for weekend / 2 for week)",
+    "Evening outfit (1 for weekend / 2 for week)",
+    "Comfortable heels/flats","Sun hat"]
+  const extraItems = [...mockSelectedItems, "Cross-body bag", "Umbrella"]
 
   test("you select all correct items, displays the result", () => {
     render(<Result travel={mockCity} checkedItems={mockSelectedItems} />)
-    const showResult = screen.getByLabelText("correct")
-    expect(showResult).toBeInTheDocument()
-    expect(showResult).toHaveTextContent(/congratulation! You answered all of them correctly./i)
+    mockSelectedItems.forEach(item => {
+      if(mockCity.importantList.includes(item)){
+        const showResult = screen.getByTestId("result")
+        expect(showResult).toBeInTheDocument()
+        expect(showResult).toHaveTextContent(/congratulation! You answered all of them correctly/i)
+      }
+    })
   })
   
-  test("shows warning when user selects more than correct items", () => {
+  test("you select wrong, displays the result", () => {
     render(<Result travel={mockCity} checkedItems={mockSelectedItems} />)
-    const showWarning = screen.getByLabelText("warning")
-    expect(showWarning).toBeInTheDocument()
-    expect(showWarning).toHaveTextContent(/Warning! You got all the correct answers, but you also selected extra ones. Challenge failed/i)
+    mockSelectedItems.forEach(item => {
+      if(!mockCity.importantList.includes(item)){
+        const showWarning = screen.getByTestId("result")
+        expect(showWarning).toBeInTheDocument()
+        expect(showWarning).toHaveTextContent(/challenge failed/i)
+      }
+    })
+
+    const correctItems = mockSelectedItems.filter(item => 
+      mockCity.importantList.includes(item)
+    )
+    const score = correctItems.length / mockCity.importantList.length 
+    expect(score).toHaveTextContent(score.toString())
+  })
+
+  test("select all correct items but select extra, displays the result", () => {
+     render(<Result travel={mockCity} checkedItems={mockSelectedItems} />)
+     const correctItems = mockSelectedItems.filter(item => 
+      mockCity.importantList.includes(item)
+    )
+    if(extraItems.length > correctItems.length){
+      const showWarning = screen.getByTestId("result")
+      expect(showWarning).toHaveTextContent(/warning! you got all the correct answers, but you also selected extra ones.challenge failed/i)
+    }
   })
 
   test("the user hasn't played yet", () => {
-    render(<Result travel={mockCity} checkedItems={null} />)
+    render(<Result travel={mockCity} checkedItems={[]} />)
     const noResult = screen.queryByTestId("result")
     expect(noResult).not.toBeInTheDocument()
-   })
+  })
 
 })
